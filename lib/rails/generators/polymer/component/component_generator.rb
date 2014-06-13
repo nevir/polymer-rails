e7838c51a8b3cb4688af1d424fe65a7f32683772
Ian MacLeod <imac@google.com>
2014-06-13 09:33:25 -0700

    Support for attribute generation

--- lib/rails/generators/polymer/component/component_generator.rb
+++ lib/rails/generators/polymer/component/component_generator.rb
@@ -15,18 +15,21 @@ module Polymer
       def initialize(*args)
         super
 
+        rails_options = ::Rails::Generators.options[:rails]
+        @js_engine = rails_options[:javascript_engine] || :js
+        @css_engine = rails_options[:stylesheet_engine] || :css
+        @tpl_engine = rails_options[:template_engine] || :erb
+
+        attributes.each do |attribute|
+          attribute.name = canonical_property_name(attribute.name)
+          attribute.type = canonical_property_type(attribute.type)
+        end
+
         global_config = ::Rails.application.config.polymer
-
-        generator_options = ::Rails::Generators.options[:rails]
-        @js_engine = generator_options[:javascript_engine] || :js
-        @css_engine = generator_options[:stylesheet_engine] || :css
-        @tpl_engine = generator_options[:template_engine] || :erb
-
         @component_root = global_config.component_root
         @tag_name = "#{global_config.tag_prefix}-#{name.underscore.dasherize}"
       end
 
-      # TODO(imac): Generate proper stylesheet based on stylesheet generator.
       def create_component_files
         component_template '%file_name%.html', @tpl_engine
         component_template '%file_name%.css', @css_engine
@@ -40,6 +43,34 @@ module Polymer
       end
 
       protected
+
+      def canonical_property_name(name)
+        name.underscore.camelize(:lower)
+      end
+
+      def canonical_property_type(type)
+        case type
+        when :number, :integer, :float, :decimal then :number
+        when :string, :text then :string
+        when :boolean, :binary then :boolean
+        when :date, :time, :datetime, :timestamp then :date
+        else type
+        end
+      end
+
+      def default_property_value(property)
+        case property.type
+        when :number  then '0'
+        when :string  then "''"
+        when :boolean then 'false'
+        when :date    then 'new Date(0)'
+        # TODO(imac): Is {value: *} still necessary?
+        when :object  then '{value: {}}'
+        when :array   then '{value: []}'
+        # TODO(imac): Is string the right default?
+        else "''"
+        end
+      end
 
       def script_source(script_type = @js_engine)
         ext = @js_engine == :js ? '.js' : ".js.#{@js_engine}"
